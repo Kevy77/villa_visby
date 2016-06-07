b318ff23b777e01cda5a5c8b8185cea0ef498089
Kevy77 <kevy@hkcreativity.com>
2016-06-07 14:45:57 +0200

    //

--- Scripts/main.js
+++ Scripts/main.js
@@ -157,6 +157,11 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+  $('#tables').hide();
+  $('#fauteuil').hide();
+  $('#mob_ext').hide();
+  $('#buffet').hide();
+  $('#deco').hide();
   $('#villa2').fadeIn(2100);
   $(".hamburger").on("click",function(){
     $(".menu").slideToggle();
@@ -172,8 +177,75 @@ $(document).ready(function(){
     });
     $('.btn__click_cross').click(function(e){
         e.preventDefault();
-        $("#content-2").slideUp( 300 );
-        $("#content-1").show( 1000 );
+        $("#content-2").slideUp( 300);
+        $("#content-1").show(1000);
+    });
+    $('#canapes_btn').click(function(e){
+        e.preventDefault();
+        $( ".design__4" ).replaceWith('<h1 class="design__4">nos canapés</h1>');
+        $('#canapes').slideDown(300);
+        $('#fauteuil').slideUp(300);
+        $('#tables').slideUp(300);
+        $('#mob_ext').slideUp(300);
+        $('#buffet').slideUp(300);
+        $('#deco').slideUp(300);
+
+    });
+    $('#fauteuils_btn').click(function(e){
+        e.preventDefault();
+        $( ".design__4" ).replaceWith('<h1 class="design__4">nos fauteuils</h1>');
+        $('#canapes').slideUp(300);
+        $('#fauteuil').slideDown(300);
+        $('#tables').slideUp(300);
+        $('#mob_ext').slideUp(300);
+        $('#buffet').slideUp(300);
+        $('#deco').slideUp(300);
+
+    });
+    $('#tables_btn').click(function(e){
+        e.preventDefault();
+        $( ".design__4" ).replaceWith('<h1 class="design__4">nos tables</h1>');
+        $('#canapes').slideUp(300);
+        $('#fauteuil').slideUp(300);
+        $('#tables').slideDown(300);
+        $('#mob_ext').slideUp(300);
+        $('#buffet').slideUp(300);
+        $('#deco').slideUp(300);
+    });
+    $('#mob_ext_btn').click(function(e){
+        e.preventDefault();
+        $( ".design__4" ).replaceWith('<h1 class="design__4">mobilier d&apos;exterieur</h1>');
+        $('#canapes').slideUp(300);
+        $('#fauteuil').slideUp(300);
+        $('#tables').slideUp(300);
+        $('#mob_ext').slideDown(300);
+        $('#buffet').slideUp(300);
+        $('#deco').slideUp(300);
+
+
+    });
+    $('#buffet_btn').click(function(e){
+        e.preventDefault();
+        $( ".design__4" ).replaceWith('<h1 class="design__4">nos buffets</h1>');
+        $('#canapes').slideUp(300);
+        $('#fauteuil').slideUp(300);
+        $('#tables').slideUp(300);
+        $('#mob_ext').slideUp(300);
+        $('#buffet').slideDown(300);
+        $('#deco').slideUp(300);
+
+
+    });
+    $('#deco_btn').click(function(e){
+        e.preventDefault();
+        $( ".design__4" ).replaceWith('<h1 class="design__4">nos décorations</h1>');
+        $('#canapes').slideUp(300);
+        $('#fauteuil').slideUp(300);
+        $('#tables').slideUp(300);
+        $('#mob_ext').slideUp(300);
+        $('#buffet').slideUp(300);
+        $('#deco').slideDown(300);
+
 
     });
 
