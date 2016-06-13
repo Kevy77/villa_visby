89ab83e757f88f3f82545ce1b67bd6d8c668a4c5
Kevy77 <kevy@hkcreativity.com>
2016-06-13 22:09:25 +0200

    //

--- Scripts/main.js
+++ Scripts/main.js
@@ -134,20 +134,6 @@ $(window).scroll(function(){
 
 $(document).ready(function(){
   $('#villa').fadeIn(2000);
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
 });
 
 $(document).ready(function(){
